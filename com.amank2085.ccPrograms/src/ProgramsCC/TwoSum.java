4a7b0aae3e09d660f92023873d276333ab8ebb32
Amank2085 <kumar.aman2085@gmail.com>
2025-01-19 19:36:57 +0530

    TwoSum

--- com.amank2085.ccPrograms/src/ProgramsCC/TwoSum.java
+++ com.amank2085.ccPrograms/src/ProgramsCC/TwoSum.java
@@ -1,6 +1,9 @@
 package ProgramsCC;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
 
 public class TwoSum {
 
@@ -32,11 +35,64 @@ public class TwoSum {
         return null;
     }
 
+//    using HashMap Two pass
+    public int [] twoSumMapTwoPass(int[] nums,int target)
+    {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i =0; i < nums.length; i++)
+        {
+            map.put(nums[i],i);
+        }
+
+        for (int i =0; i < nums.length; i++)
+        {
+            int findMe = target - nums[i];
+            if(map.containsKey(findMe) && map.get(findMe) != i)
+                return new int[]{i, map.get(findMe)};
+        }
+        return null;
+    }
+
+    public int [] twoSumMapSinglePass(int[] nums,int target)
+    {
+        Map<Integer,Integer> map = new HashMap<>();
+        for (int i =0; i < nums.length; i++)
+        {
+            int findMe = target - nums[i];
+            if(map.containsKey(findMe) && map.get(findMe) != i) {
+                return new int[]{i, map.get(findMe)};
+            }
+            map.put(nums[i],i);
+        }
+
+        return null;
+    }
+
+//    using streams
+    public static int[] twoSumStream(int[] nums, int target)
+    {
+        Map<Integer, Integer> numMap = new HashMap<>();
+        return IntStream.range(0, nums.length) .filter(
+                i -> { int complement = target - nums[i];
+                    if (numMap.containsKey(complement))
+                    {
+                        return true;
+                    }
+                    else {
+                        numMap.put(nums[i], i);
+                        return false;
+                    }}) .mapToObj(i -> new int[]{numMap.get(target - nums[i]), i}) .findFirst() .orElse(null);
+    }
+
     public static void main(String[] args) {
-        int nums[] = {2, 11, 5 , 4};
+        int nums[] = {2,7,11,15};
         int target = 9;
         TwoSum sum = new TwoSum();
         int [] result = sum.twoSum(nums,target);
-        System.out.println(Arrays.toString(result));
+        int [] result1 = sum.twoSumMapTwoPass(nums, target);
+        int [] result2 = sum.twoSumMapSinglePass(nums,target);
+        System.out.println("Brute Force O(n^2) : "+Arrays.toString(result));
+        System.out.println("Map Two Pass O(n) : "+Arrays.toString(result1));
+        System.out.println("Map Single Pass O(n) : "+Arrays.toString(result2));
     }
 }
