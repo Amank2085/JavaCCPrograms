c089bd17065bed3a5d5004ceebeca5780070b191
Amank2085 <kumar.aman2085@gmail.com>
2025-02-02 20:56:44 +0530

    StringManipulation

--- com.amank2085.ccPrograms/src/ProgramsCC/StringManipulation.java
+++ com.amank2085.ccPrograms/src/ProgramsCC/StringManipulation.java
@@ -1,5 +1,6 @@
 package ProgramsCC;
 
+import java.util.Arrays;
 import java.util.OptionalInt;
 import java.util.stream.IntStream;
 
@@ -43,6 +44,39 @@ public class StringManipulation {
         } else {
             System.out.println("The character 's' does not occur " + occurrence + " times in the string.");
         }
+
+        //String Comparison
+        String str1 = "The rains Have started here soon";
+        System.out.println(str.equals(str1));
+        System.out.println(str.equalsIgnoreCase(str1));
+
+        //substring
+        System.out.println(str.substring(0,9));
+
+        //trim
+        String s = "           Hello World          ";
+        System.out.println(s.trim());
+        System.out.println(s.replace(" ",""));
+
+        //split
+        String test = "Hello_World_Test_Selenium";
+        String[] split = test.split("_");
+       for(String s1: split) System.out.println(s1);
+
+       //Concat
+        String s2 = "cares";
+        System.out.println(s2.concat("s"));
+
+        String x = "Hello";
+        String y = "World";
+        int a = 100;
+        int b = 200;
+
+        System.out.println(x+y);
+        System.out.println(a+b);
+        System.out.println(x+y+a+b);
+        System.out.println(a+b+x+y);
+        System.out.println(x+y+(a+b));
     }
 
 }
